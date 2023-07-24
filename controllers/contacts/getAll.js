98d2f3e8f7ef4aaa3ecd30d54ccc132518a487b5
Serhii-Buha <serhii.buga@gmail.com>
2023-07-24 10:18:54 +0300

    added comments filter favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -11,6 +11,14 @@ exports.getAll = async (req, res, next) => {
 
     // Если запрос содержит параметр favorite, добавьте его в фильтр
     if (favorite !== undefined) filter.favorite = favorite === "true";
+    //     Здесь проверяется, задан ли параметр favorite в строке запроса (req.query). Если он задан, то в объект filter добавляется свойство favorite. Значение favorite будет true если в строке запроса favorite равен "true", и false в противном случае. Это связано с тем, что все параметры в строке запроса передаются как строки, поэтому мы сравниваем его со строкой "true".
+
+    // Теперь объект filter может выглядеть следующим образом:
+
+    // Если favorite не указан в строке запроса: { owner: 'some_user_id' }.
+    // Если favorite указан в строке запроса и равен "true": { owner: 'some_user_id', favorite: true }.
+    // Если favorite указан в строке запроса и не равен "true": { owner: 'some_user_id', favorite: false }.
+    // Этот объект filter затем используется в запросе к MongoDB для выборки документов из коллекции Contact:
 
     const result = await Contact.find(filter, "-createdAt -updatedAt", {
       skip,
